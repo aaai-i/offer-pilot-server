6ca5d03ea2a201ac7e9ab8d52f7e8f652a38ef65
彭彭 <appa21@qq.com>
2026-04-07 11:54:39 +0800

    init: 初始化Spring Boot项目

---
+++ src/main/java/org/example/offerpilotserver/OfferPilotServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.offerpilotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferPilotServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfferPilotServerApplication.class, args);
+    }
+
+}
